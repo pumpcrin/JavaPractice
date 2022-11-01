77d8947086275e8facaa4d475d8f9853c21a6efc
pumpcrin <shacsi_tr1835@yahoo.com>
2022-11-01 19:24:38 +0900

    6章終了.

---
+++ 6/Main.java
@@ -0,0 +1,10 @@
+import comment.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        comment.Zenhan.doWarusa();
+        comment.Zenhan.doTogame();
+        comment.Kouhan.callDeae();
+        comment.Kouhan.showMondokoro();
+    }
+}
